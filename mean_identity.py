a94c3fd1e35ce2d6d8329e5f5e3cc2c9f092c603
Tao Wang <48007630+daizigege@users.noreply.github.com>
2025-09-13 15:40:07 +0200

    Update mean_identity.py

--- mean_identity.py
+++ mean_identity.py
@@ -1,7 +1,3 @@
-# Copyright (C) 2018 Elvis Yu-Jing Lin <elvisyjlin@gmail.com>
-#
-# This work is licensed under the MIT License. To view a copy of this license,
-# visit https://opensource.org/licenses/MIT.
 
 # encoding: utf-8
 import argparse
